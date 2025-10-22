abaae51cada5814d7c680b7e6c2d138371e02d59
NaFo <81775542+NaFo61@users.noreply.github.com>
2025-10-22 03:23:51 +0300

    добавил логотип, кастомный index.html в unfold (#8)

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,27 +1,33 @@
-# users/admin.py
 from django.contrib import admin
 from django.contrib.auth.models import Group
 from unfold.admin import ModelAdmin
 
-from .models import User
+from users.models import User
 
-# Полностью отключаем Group из админки
 admin.site.unregister(Group)
 
 
 @admin.register(User)
 class CustomUserAdmin(ModelAdmin):
-    list_display = [
+    list_display = (
         "email",
         "phone",
         "first_name",
         "last_name",
         "role",
         "is_active",
-    ]
+    )
 
-    list_filter = ["role", "is_active"]
-    search_fields = ["email", "phone", "first_name", "last_name"]
+    list_filter = (
+        "role",
+        "is_active",
+    )
+    search_fields = (
+        "email",
+        "phone",
+        "first_name",
+        "last_name",
+    )
 
     fieldsets = (
         ("Основная информация", {"fields": ("email", "phone", "password")}),
@@ -34,27 +40,3 @@ class CustomUserAdmin(ModelAdmin):
             {"fields": ("role", "is_active", "is_staff", "is_superuser")},
         ),
     )
-
-
-# Убираем раздел "Пользователи и группы" из sidebar
-UNFOLD = {
-    "SITE_TITLE": "My Awesome Admin",
-    "SITE_HEADER": "My Administration",
-    # ... остальные настройки unfold
-    "SIDEBAR": {
-        "show_search": True,
-        "show_all_applications": False,  # Показывать только нужные приложения
-        "navigation": [
-            {
-                "title": "Пользователи",
-                "items": [
-                    {
-                        "title": "Пользователи",
-                        "icon": "person",
-                        "link": "admin:users_user_changelist",
-                    },
-                ],
-            },
-        ],
-    },
-}
